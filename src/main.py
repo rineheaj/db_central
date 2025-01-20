fd9d9eb9abf675cc0d31a8cb9b451de226995d6f
rineheaj <joshtrineheart@gmail.com>
2025-01-19 18:07:20 -0600

    On branch master Your branch is up to date with 'origin/master'. Changes to be committed: 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
-from sql_model_practice.db.config.config_db import(
+from sql_model_practice.db.config.config_db import (
     init_db,
 )
 
 
-
 def main():
-   init_db()
-   print("DATABASE STARTED")
-   
-
+    init_db()
+    print("DATABASE STARTED")
 
 
 if __name__ == "__main__":
-   main()
-
+    main()
